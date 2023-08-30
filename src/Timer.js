486f1fcc7358d1049f54971204a2514de09121ed
Daniel <daxminer@hotmail.com>
2023-08-29 19:51:38 -0500

    Primer commit, modificacion de app.js y cracion de timer.js

---
+++ src/Timer.js
@@ -0,0 +1,15 @@
+import logo from './logo.svg';
+import './App.css';
+import React from 'react';
+import React, { useState } from 'react';
+
+const Timer = () => {
+
+  return(
+<>
+
+</>
+    );
+}
+
+export default Timer;
